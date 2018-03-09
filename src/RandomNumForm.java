ed916e00a8caff983e5d213d91dc4f4872f649c0
dytav9540 <dytav9540@7J7N6Y1.Student.UGDSB.ED>
2018-03-09 14:43:27 -0500

    This is my first commit

---
+++ src/RandomNumForm.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class RandomNumForm extends javax.swing.JFrame {
+
+    
+    public RandomNumForm() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        labelTitle = new javax.swing.JLabel();
+        buttonGenerate = new javax.swing.JButton();
+        labelHeaderprogram = new javax.swing.JLabel();
+        labelHeaderEach = new javax.swing.JLabel();
+        labelMessage = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        labelTitle.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        labelTitle.setText("Random Number Generator");
+
+        buttonGenerate.setText("Generate");
+        buttonGenerate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonGenerateActionPerformed(evt);
+            }
+        });
+
+        labelHeaderprogram.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        labelHeaderprogram.setText("This program will generate a random number from 5 - 20");
+
+        labelHeaderEach.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        labelHeaderEach.setText("Each time you hit this button");
+
+        labelMessage.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        labelMessage.setText("The number that was generated was: ");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(102, 102, 102)
+                        .addComponent(labelTitle))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addComponent(labelHeaderprogram))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(192, 192, 192)
+                        .addComponent(buttonGenerate))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(139, 139, 139)
+                        .addComponent(labelHeaderEach))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(116, 116, 116)
+                        .addComponent(labelMessage)))
+                .addContainerGap(71, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(labelTitle)
+                .addGap(39, 39, 39)
+                .addComponent(labelHeaderprogram)
+                .addGap(18, 18, 18)
+                .addComponent(labelHeaderEach)
+                .addGap(48, 48, 48)
+                .addComponent(buttonGenerate)
+                .addGap(32, 32, 32)
+                .addComponent(labelMessage)
+                .addContainerGap(56, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void buttonGenerateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonGenerateActionPerformed
+       double highNum, lowNum, randomNum;// Declares doubles called highNum, lowNum, randomNum
+
+       highNum = 16;// Assigns 16 to highNum
+       lowNum = 5; // Assigns 5 to lowNum
+       randomNum = ( Math.floor(Math.random() * highNum + lowNum));// Assigns random number from 5 to 20 to randomNum 
+       labelMessage.setText("The number that was generated was: " + randomNum);// Sets the number that was generated was: and randomNum to the labelMessage
+    }//GEN-LAST:event_buttonGenerateActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(RandomNumForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(RandomNumForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(RandomNumForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(RandomNumForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new RandomNumForm().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buttonGenerate;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel labelHeaderEach;
+    private javax.swing.JLabel labelHeaderprogram;
+    private javax.swing.JLabel labelMessage;
+    private javax.swing.JLabel labelTitle;
+    // End of variables declaration//GEN-END:variables
+}
